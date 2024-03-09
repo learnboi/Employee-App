0635eb017559b7ce14267e484281ecd6a1a333f4
learnboi <chaitanyagayakwad100@gmail.com>
2024-03-09 13:05:31 +0530

    HttpSEssionManagement

---
+++ src/main/java/com/example/assignmenthttpsession/Userdata.java
@@ -0,0 +1,54 @@
+package com.example.assignmenthttpsession;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@WebServlet("/userdata")
+public class Userdata extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter pw =response.getWriter();
+        try {
+                HttpSession session = request.getSession(false);
+                String storedUsername = (String) session.getAttribute("username");
+                String url = "jdbc:mysql://localhost:3306/login";
+                String user = "root";
+                String pass = "root";
+                Connection conn = DriverManager.getConnection(url, user, pass);
+                Statement statement = conn.createStatement();
+                String Sql = "SELECT * FROM userdata WHERE username='" + storedUsername + "'";
+                ResultSet resultSet = statement.executeQuery(Sql);
+                List<Employee> userlist = new ArrayList<>();
+                while (resultSet.next()) {
+                    String username = resultSet.getString("username");
+                    String name = resultSet.getString("name");
+                    int age = resultSet.getInt("age");
+                    String email = resultSet.getString("email");
+                    long phone = resultSet.getLong("phone");
+                    session.setAttribute("username",username);
+                    session.setAttribute("name",name);
+                    session.setAttribute("age",age);
+                    session.setAttribute("email",email);
+                    session.setAttribute("phone",phone);
+                    RequestDispatcher rd = request.getRequestDispatcher("profile.jsp");
+                    rd.forward(request,response);
+                }
+            }catch (Exception e){
+                pw.write("<h1>" + e.getLocalizedMessage() + "</h1>");
+            }
+    }
+
+}
+
---
+++ src/main/webapp/profile.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: chait
+  Date: 2024-03-09
+  Time: 12:47 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Profile</title>
+</head>
+<body>
+
+<h1>Hello,<%=session.getAttribute("name")%> Welcome to Profile</h1>
+
+</body>
+</html>
